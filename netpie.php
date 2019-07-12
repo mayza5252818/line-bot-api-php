1642fd3afc4982e7f9bf477ab091912984e24e2e
mayza5252818 <mayza5252818@gmail.com>
2019-07-12 14:53:23 +0700

    Update netpie.php

--- netpie.php
+++ netpie.php
@@ -1,8 +1,8 @@
  <?php
 	function pubMqtt($topic,$msg){
-		$APPID= "LineBotLED/"; //enter your appid
-		$KEY = "OCbJNzksoFmEdXC"; //enter your web key
-		$SECRET = "UwTwehyr7ikZnXcJv7acvwTQq"; //enter your secret
+		$APPID= "TanasLineBotLED/"; //enter your appid
+		$KEY = "oI93OcmPuVdZqs9"; //enter your web key
+		$SECRET = "oI93OcmPuVdZqs9:nYTgmNEa3HhiJQs8ANoZ2natY"; //enter your secret
 		$Topic = "$topic";
 		if($msg =="on"){
 			$msg = "1";
