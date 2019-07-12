97e5948d169b73fdd68ff156d9bad61071842f62
mayza5252818 <mayza5252818@gmail.com>
2019-07-12 14:57:13 +0700

    Update line.php

--- line.php
+++ line.php
@@ -1,7 +1,7 @@
 <?php
 
 $API_URL = 'https://api.line.me/v2/bot/message/reply';
-$ACCESS_TOKEN = 'IG+YXYoYXHnnyNt5Ugfp7c7ME85Cndf63UaJkZICfoa6bDZpEooYURSFFCU7PV+ALGK1yAUxiW+vFrHZJFNIyEmHsWLrx60Y6XMf4R7pr6+mfhGdbxO8xlpxGjPz0uGKb+J4akxmJQCIL0GikF5i6AdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
+$ACCESS_TOKEN = 'xNSgpb4BNjuK5sdbinYo4AU0odx5mHOuth+jcWr46JRPcgSHKpfp94vPaSCLqTXlTSbN7N12bT9J/15Bm3NbUPpBnAuYmPVjWyCjloKd+TDcJj1/5T4dV9cKiq4J6ONc8WdJzBZdZrQfZnqE8jU8KQdB04t89/1O/w1cDnyilFU='; // Access Token ค่าที่เราสร้างขึ้น
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
